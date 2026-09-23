7726a063f1df99ab7e99b4abb27957a191629a97
RedHead <alex1980zyr@gmail.com>
2026-09-24 00:00:47 +0300

    Add converter for length and mass

--- src/toolkit/converter.py
+++ src/toolkit/converter.py
@@ -1,7 +1,11 @@
 measures = {
-    (a,"мм"): (a/1000,'м'),
-    (a,"cм"): (a/100,"м"),
-    (a,"дм"): (a/10,"м"),
-    (a,"км"): (a*1000,"м"),
-    
+    'mm': 0.001,
+    'cm': 0.01,
+    'm', 1,
+    'dm': 0.1,
+    'km': 1000,
+    'kg': 1,
+    'g': 0.001
 }
+def convert_len(value,from_measure,to_measure):
+    return value * measures(from_measures) / measures(to_measures)
